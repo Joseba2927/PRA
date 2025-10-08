cacc994bf48b8aea9d0ff2e8b2c674d9547b369a
joseba <jnarvil@upv.edu.es>
2025-10-08 10:17:43 +0200

    Añadido quicksort a DyH

--- DyV.h
+++ DyV.h
@@ -21,4 +21,25 @@ int BusquedaBinaria(const std::vector<int>& v, int x, int ini, int fin) {
         return BusquedaBinaria(v, x, medio + 1, fin);
     }
 }
+void QuickSort(vector<int>& v, int ini, int fin) {
+    if (ini < fin) {
+        int pivot = Partition(v, ini, fin);
+        QuickSort(v, ini, pivot - 1);
+        QuickSort(v, pivot + 1, fin);
+    }
+}
+
+int Partition(vector<int>& v, int ini, int fin) {
+    int  pivot = v[fin];    
+    int i = ini;
+
+    for (int j = ini; j < fin; ++j) {
+        if (v[j] <= pivot) {
+            swap(v[i], v[j]);
+            i++;
+        }
+    }
+    swap(v[i], v[fin]);
+    return i;
+}
 
